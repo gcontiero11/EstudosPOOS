6eba63111d6d35c6e08082a2b37e35fce2a518c5
gcontiero11 <gustcontiero@gmail.com>
2023-12-29 20:45:18 -0300

    Fazendo Aula3

--- Aula3/src/Exercicios.java
+++ Aula3/src/Exercicios.java
@@ -1,2 +1,26 @@
 public class Exercicios {
+    int exercicioEscolhido;
+
+    public Exercicios(int exercicioEscolhido){
+        this.exercicioEscolhido = exercicioEscolhido;
+    }
+
+    public void selecionaExercicio(){
+        if(exercicioEscolhido == 1){
+            this.ex001();
+        } else if (exercicioEscolhido == 2) {
+            this.ex002();
+        } else if (exercicioEscolhido == 0) {
+            System.out.println("Saindo...");
+        } else{
+            System.out.println("Exercicio não encontrado!");
+        }
+
+    }
+    private void ex001(){
+        System.out.println("Executando exercício 1");
+    }
+    private void ex002(){
+        System.out.println("Executando exercício 2");
+    }
 }
--- Aula3/src/Main.java
+++ Aula3/src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Escolha o exercicio que deseja executar: ");
+        System.out.println("Ex001...[1]");
+        System.out.println("Ex002...[2]");
+        System.out.println("Sair....[0]");
+        System.out.println("--------->");
+        int escolha = leitor.nextInt();
+        Exercicios exs = new Exercicios(escolha);
+        exs.selecionaExercicio();
     }
 }
