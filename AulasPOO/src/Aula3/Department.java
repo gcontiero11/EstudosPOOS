ccb0e4748c0459bbabca83a5dc3088a74929c2e9
gcontiero11 <gustcontiero@gmail.com>
2024-01-27 15:52:07 -0300

    Ex1 aula 3

---
+++ AulasPOO/src/Aula3/Department.java
@@ -0,0 +1,25 @@
+package Aula3;
+
+public class Department {
+    private String name;
+    private int code;
+    private String location;
+    private int phoneExtension;
+    private double budget;
+
+    public void addEmployee (Employee e){
+    }
+    public void removeEmployee (Employee e){
+    }
+    public void removeEmployee (long id){
+    }
+    public void listAllEmployees(){
+    }
+    public int sizeOfEmployees(){
+        return -1;
+    }
+    public Employee getEmployee(long id){
+        return null;
+    }
+
+}
---
+++ AulasPOO/src/Aula3/Employee.java
@@ -0,0 +1,13 @@
+package Aula3;
+
+public class Employee {
+    private String name;
+    private long id;
+    private double salary;
+    private String jobTitle;
+
+    public void printState(){
+    }
+    public void getAnnualSalary(){
+    }
+}
